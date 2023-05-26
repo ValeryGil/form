3f0719aa05121587f98f603da76a2528a72c628b
ValeryGil <valery.gil@icloud.com>
2023-05-26 12:23:51 +0300

    Changed types of fields class FormDate

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,8 +10,8 @@ public class Main {
         post.subscription = true;
 
         post.birthday = new FormDate();
-        post.birthday.day = "13";
-        post.birthday.month = "6";
-        post.birthday.year = "1999";
+        post.birthday.day = 13;
+        post.birthday.month = 6;
+        post.birthday.year = 1999;
     }
 }
